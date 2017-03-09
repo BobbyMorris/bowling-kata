361b6ab709398b4981d035f1b1ae21eb7d1baa38
Robert Morris <bobbymorris@gmail.com>
2017-03-08 23:15:24 -0500

    Fourth test - a strike

--- BowlingGame/BowlingGameTests.cs
+++ BowlingGame/BowlingGameTests.cs
@@ -49,6 +49,21 @@ namespace BowlingGame
             Assert.AreEqual(16, g.Score());
         }
 
+        [Test]
+        public void TestOneStrike()
+        {
+            RollStrike();
+            g.Roll(3);
+            g.Roll(4);
+            RollMany(16, 0);
+            Assert.AreEqual(24, g.Score());
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
+        }
+
         private void RollSpare()
         {
             g.Roll(5);
